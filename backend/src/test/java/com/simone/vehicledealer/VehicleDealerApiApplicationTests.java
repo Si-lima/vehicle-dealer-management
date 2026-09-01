2ce6dd27219bd81e306d525de425f9679c6b87a3
Simone Goncalves Lima Costa <simonelimacosta11@gmail.com>
2026-09-01 17:44:05 -0300

    chore: initialize backend and PostgreSQL

---
+++ backend/src/test/java/com/simone/vehicledealer/VehicleDealerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simone.vehicledealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDealerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
